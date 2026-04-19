26457a2067a78051e789884d2a7e41d01714c01a
John Q. Herman <john@raccoonlagoon.com>
2026-04-19 00:03:14 -0400

    update desploy.sh for docker

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,6 @@ cd /var/www/youfoundmybag
 git fetch origin
 git reset --hard origin/master
 
-npm ci
+docker compose up --build -d
 
-npm run build
-
-pm2 restart ecosystem.config.json --update-env || pm2 start ecosystem.config.json
-pm2 save
+docker image prune -f
